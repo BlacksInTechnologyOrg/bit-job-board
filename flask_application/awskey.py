2579a124ff527abb07f287019a0a13214d1e6210
Herman Haggerty <dubh3124@gmail.com>
2018-04-17 00:07:23 -0700

    Removed unused aws keys
    Remove unused AWS key and created new IAM user and s3 bucket related to bit-jobboard.

--- flask_application/awskey.py
+++ flask_application/awskey.py
@@ -1,6 +1,2 @@
-akey = "AKIAIEMHFUIQVN2PZBCQ"
-skey = "0JprJEMYjT/XO5KY5EB42jzb3f3Wvf58zb5finET"
-
-
-cdn = "http://d1ohfywmamjz3v.cloudfront.net/"
-bucket= "dubmusic01"
+cdn = "http://d3djhyh77jzjrj.cloudfront.net/"
+bucket= "bit-jobboard"
