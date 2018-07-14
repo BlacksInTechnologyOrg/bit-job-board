8f86893926744b55c29d7db1eafd4bf993e03dba
Herman Haggerty <dubh3124@gmail.com>
2018-07-14 10:15:43 -0700

    Configured Flask to serve vuejs app

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
+
+FRONTEND_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'front-end'))
+if not os.path.exists(FRONTEND_DIR):
+    raise Exception(
+        'Client App directory not found: {}'.format(FRONTEND_DIR))
+
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__,
+                static_folder=os.path.join(FRONTEND_DIR, 'dist'),
+                template_folder=os.path.join(FRONTEND_DIR, 'dist')
+                )
 
+    print(app.static_folder)
     app.config.from_object('flask_app.config.app_config')
     app.logger.info("Config: %s" % app.config['ENVIRONMENT'])
 
@@ -42,5 +53,10 @@ def create_app():
     # app.cli
     # app.cli.add_command(resetdb)
     # app.cli.add_command(populatedb)
+
+    @app.route('/', defaults={'path': ''})
+    @app.route('/<path:path>')
+    def catch_all(path):
+        return render_template("index.html")
 
     return app
